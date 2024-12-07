d03529d90c9fd05e6f3d588a8b80e73beeea5787
Rutuja Nikam <rutuja.nikam@haptiq.com>
2024-12-07 19:24:31 +0530

    React project

---
+++ React/movie/src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './styles.css'
+import { useState,useEffect } from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import MoviesGrid from './components/MoviesGrid';
+import Watchlist from './components/Watchlist';
+import {BrowserRouter as Router, Route, Routes ,Link} from 'react-router-dom';
+
+
+export default function App(){
+
+  const [movies,setMovies]=useState([]);
+  const [watchlist,setWatchlist]=useState([]);
+
+  useEffect(()=>{
+    fetch("movies.json")
+     .then(response=>response.json())
+     .then(data=>setMovies(data))
+},[]);
+
+
+ const toggleWatchlist =(movieId)=>{
+     setWatchlist(prev=>
+      prev.includes(movieId)? prev.filter(id=>id!==movieId):[...prev,movieId]
+     )
+ }
+ 
+    return (
+      <div className="App">
+        <div className='container'>
+          <Header></Header>
+        </div>
+
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home </Link>
+              </li>
+              <li>
+                <Link to="/watchlist">WatchList</Link>
+              </li>
+            </ul>
+
+
+          </nav>
+          <Routes>
+            <Route path="/" element={<MoviesGrid  watchlist={watchlist} movies={movies} toggleWatchlist={toggleWatchlist}/>}> </Route>
+            <Route path="/watchlist" element={<Watchlist watchlist={watchlist} movies={movies} toggleWatchlist={toggleWatchlist}/>}> </Route>
+          </Routes>
+        </Router>
+
+       
+       <Footer></Footer>
+      </div>
+    );
+  }
+
+
+
---
+++ React/movie/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles.css';
+
+export default function Header(){
+       return(
+
+        <div className='header'>
+            <img className='logo' src='logo.png' alt='moviedux'></img>
+            <h2 className='app-subtitle'>It's time for popcorn! find your next movie here.</h2>
+        </div>
+       );
+}
